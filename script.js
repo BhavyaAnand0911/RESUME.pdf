508e90cf06cf1cc1cae0aa5d9e4c5ed984732cb6
Bhavya-Anand <bhavyaanand0911@gmail.com>
2023-03-25 03:45:02 +0530

    first commit to the project

---
+++ script.js
@@ -0,0 +1,44 @@
+let pageNo=[".container",".pageOne",".pageTwo",".pageThree",".pageFour"];
+let iname=[];
+let deg=[];
+let grade=[];
+var pageCount=0;
+var eduCount=0;
+function start(){
+    // alert("working");
+    $(pageNo[a]).hide();
+    a++;
+    $(pageNo[a]).show();
+}
+a=0;
+function nextPage(){
+    $(pageNo[a]).hide();
+    a++;
+    $(pageNo[a]).show();
+}
+
+function addEdu(){
+    try{
+        iname[eduCount]=document.forms['form1']['iname'].value;
+        deg[eduCount]=document.forms['form1']['deg'].value;
+        grade[eduCount]=document.forms['form1']['grade'].value;
+    }
+    catch{
+        console.log('ERROR OCCURED');
+    }
+    eduCount++;
+    // console.log(eduCount);
+    // console.log(iname[eduCount]);
+    // console.log(deg[eduCount]);
+    // console.log(grade[eduCount]);
+    $('.form2')[0].reset();
+}
+
+function addProject(){
+    $('.form3')[0].reset();
+    $('textarea').val('');
+} 
+function addExp(){
+    $('.form4')[0].reset();
+    $('textarea').val('');
+}
