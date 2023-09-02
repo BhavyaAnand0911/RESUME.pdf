52a43c870125a501d888b0b0f26e1b1c930f2b7c
Bhavya-Anand <bhavyaanand0911@gmail.com>
2023-09-03 02:15:47 +0530

    Initital Commit

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-const http=require('http');
-const fs=require('fs');
-
-const server=http.createServer((req,res)=>{
-    fs.readFile('./index.html','utf-8',(error,data)=>{
-       if(error) {console.log('error oeccured'); return;}
-       else{
-        if(req.url==='/'){
-            res.end(data);
-        }
-        else if(req.url==='/contact'){
-            res.end(data);
-        }
-        else if(req.url==='/about'){
-            res.end(data);
-        }
-        else{
-            res.end('ERROR 404 : FILE NOT FOUND');
-        }
-       } 
-    });
-});
-
-server.listen(3000,()=>{
-    console.log('SERVER RUNNING');
-})
+const PORT=4000;
+const hostname='localhost';
